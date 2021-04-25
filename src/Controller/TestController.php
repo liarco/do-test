23782b3c416f2c4dbe4f1a8ec35eba251dc0ea35
Marco Lipparini <developer@liarco.net>
2021-04-25 15:15:47 +0200

    Test form

---
+++ src/Controller/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\TestFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/", name="test")
+     */
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(TestFormType::class);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
+
+        return $this->render('test/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
